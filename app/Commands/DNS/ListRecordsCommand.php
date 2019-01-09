78c768f2d2b8373d7c94e768d92fa6a892b6c7db
Zaher Ghaibeh <z@zah.me>
2019-01-09 14:57:53 +0300

    some info about the DNS records

---
+++ app/Commands/DNS/ListRecordsCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Commands\DNS;
+
+use Cloudflare\API\Endpoints\DNS;
+use Cloudflare\API\Endpoints\Zones;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class ListRecordsCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'dns:list-records
+                            {domain : The domain name}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'List all the DNS records for a specific domain.';
+
+    /**
+     * Execute the console command.
+     *
+     * @param \Cloudflare\API\Endpoints\DNS $dns
+     * @param \Cloudflare\API\Endpoints\Zones $zones
+     * @return mixed
+     * @throws \Cloudflare\API\Endpoints\EndpointException
+     */
+    public function handle(DNS $dns, Zones $zones)
+    {
+        $zoneID = $zones->getZoneID($this->argument('domain'));
+
+        var_dump($dns->listRecords($zoneID)->result);
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
