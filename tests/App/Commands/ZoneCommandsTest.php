68ba20feea5ef825ab4d2137c491506712ae76a3
Zaher Ghaibeh <z@zah.me>
2019-02-02 17:22:50 +0300

    wip

--- tests/App/Commands/ZoneCommandsTest.php
+++ tests/App/Commands/ZoneCommandsTest.php
@@ -20,4 +20,23 @@ class ZoneCommandsTest extends TestCase
 
         $this->assertCommandCalled('zone:list');
     }
+
+    public function testZoneActivationCheckCommand(): void
+    {
+        $zone = $this->createMock(Zones::class);
+
+        $zone->method('getZoneID')
+            ->willReturn('023e105f4ecef8ad9ca31a8372d0c353');
+
+        $zone->method('activationCheck')
+            ->willReturn(true);
+
+        $this->instance(Zones::class, $zone);
+
+        $this->artisan('zone:check-activation', ['domain' => 'example.com'])
+            ->expectsOutput('We have successfully initiated another zone activation check')
+            ->assertExitCode(0);
+
+        $this->assertCommandCalled('zone:check-activation', ['domain' => 'example.com']);
+    }
 }
