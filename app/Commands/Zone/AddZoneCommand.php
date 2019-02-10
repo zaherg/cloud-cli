8e3b684b3c2bd14c7b895434bdda6ec17dbebea2
Zaher Ghaibeh <z@zah.me>
2019-02-10 20:33:49 +0300

    Refactoring

--- app/Commands/Zone/AddZoneCommand.php
+++ app/Commands/Zone/AddZoneCommand.php
@@ -2,16 +2,17 @@
 
 namespace App\Commands\Zone;
 
+use App\Traits\ZonesTrait;
 use App\Traits\CommonTrait;
 use Cloudflare\API\Endpoints\Zones;
 use GuzzleHttp\Exception\ClientException;
 use LaravelZero\Framework\Commands\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class AddZoneCommand extends Command
 {
     use CommonTrait;
+    use ZonesTrait;
+
     /**
      * The signature of the command.
      *
@@ -26,39 +27,6 @@ class AddZoneCommand extends Command
      * @var string
      */
     protected $description = 'Create a new Zone';
-
-    /**
-     * Initializes the command after the input has been bound and before the input
-     * is validated.
-     *
-     * This is mainly useful when a lot of commands extends one main command
-     * where some things need to be initialized based on the input arguments and options.
-     *
-     * @see InputInterface::bind()
-     * @see InputInterface::validate()
-     *
-     * @param \Symfony\Component\Console\Input\InputInterface   $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     */
-    protected function initialize(InputInterface $input, OutputInterface $output): void
-    {
-        $this->domain = $this->argument('domain');
-    }
-
-    /**
-     * Interacts with the user.
-     *
-     * This method is executed before the InputDefinition is validated.
-     * This means that this is the only place where the command can
-     * interactively ask for values of missing required arguments.
-     *
-     * @param \Symfony\Component\Console\Input\InputInterface   $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     */
-    protected function interact(InputInterface $input, OutputInterface $output): void
-    {
-        $this->setDomainArgument();
-    }
 
     /**
      * Execute the console command.
---
+++ app/Traits/ZonesTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+trait ZonesTrait
+{
+    /**
+     * Initializes the command after the input has been bound and before the input
+     * is validated.
+     *
+     * This is mainly useful when a lot of commands extends one main command
+     * where some things need to be initialized based on the input arguments and options.
+     *
+     * @see InputInterface::bind()
+     * @see InputInterface::validate()
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->domain = $this->argument('domain');
+    }
+
+    /**
+     * Interacts with the user.
+     *
+     * This method is executed before the InputDefinition is validated.
+     * This means that this is the only place where the command can
+     * interactively ask for values of missing required arguments.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     */
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        $this->setDomainArgument();
+    }
+}
